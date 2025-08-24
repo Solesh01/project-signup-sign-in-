ad99fd5e32bdfc8f0680643742c127d9b310f962
soleswaran <146163174+Solesh01@users.noreply.github.com>
2025-08-24 12:34:56 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,7 @@
+var x = 7;
+function result() {
+console.log("Namsate Javascript");
+
+}
+result();
+console.log(x);
---
+++ script.js
@@ -0,0 +1,4 @@
+let btn = document.getElementById("summa");
+document.addEventListener('click', (Event)=>{
+    btn.style.background = 'violet';
+})
